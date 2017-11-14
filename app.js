a0c0944f4dbe67f389e39fa339e29d55d26a36a6
Vitaliy <puga.vitaliy@gmail.com>
2017-11-14 20:49:42 +0200

    rewrite server.on(listening, function(){}

--- app.js
+++ app.js
@@ -180,27 +180,32 @@ function onError(error) {
 }
 
 server.on('listening', function () {
-  jsonfile
-    .readFile(fileVersionControl, function (err, obj) {
-      if (err) {
-        console.log('Данные для хеширования ресурсов из version.json не прочитаны');
-        console.log('Сервер остановлен');
-        process.exit(1);
-      } else {
-        app.locals.settings = {
-          suffix: obj.suffix,
-          version: obj.version
-        };
-        console.log('Данные для хеширования ресурсов из version.json прочитаны');
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+  // jsonfile
+  //   .readFile(fileVersionControl, function (err, obj) {
+  //     if (err) {
+  //       console.log('Данные для хеширования ресурсов из version.json не прочитаны');
+  //       console.log('Сервер остановлен');
+  //       process.exit(1);
+  //     } else {
+  //       app.locals.settings = {
+  //         suffix: obj.suffix,
+  //         version: obj.version
+  //       };
+  //       console.log('Данные для хеширования ресурсов из version.json прочитаны');
 
-        //если такой папки нет - создаем ее
-        if (!fs.existsSync(uploadDir)) {
-          fs.mkdirSync(uploadDir);
-        }
+  //       //если такой папки нет - создаем ее
+  //       if (!fs.existsSync(uploadDir)) {
+  //         fs.mkdirSync(uploadDir);
+  //       }
 
-        console.log('Express server started on port %s at %s', server.address().port, server.address().address);
-      }
-    });
+  //       console.log('Express server started on port %s at %s', server.address().port, server.address().address);
+  //     }
+  //   });
 });
 //-----------------------------------------------------------------------------------------------------------------------
 // module.exports = app;
